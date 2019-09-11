b923ec42c0e74b9fbf08d5fe6c045d5ae7047366
Naja Melan <najamelan@autistici.org>
2019-09-12 00:56:58 +0200

    Keep processing incoming data even after we have initiated a close handshake.

--- src/protocol/mod.rs
+++ src/protocol/mod.rs
@@ -396,7 +396,7 @@ impl WebSocketContext {
                     }
                 }
 
-                OpCode::Data(_) if !self.state.is_active() => {
+                OpCode::Data(_) if !self.state.can_read() => {
                     // No data processing while closing.
                     Ok(None)
                 }
@@ -552,6 +552,17 @@ impl WebSocketState {
     fn is_active(&self) -> bool {
         match *self {
             WebSocketState::Active => true,
+            _ => false,
+        }
+    }
+
+    /// Tell if we should process incoming data. Note that if we send a close frame
+    /// but the remote hasn't confirmed, they might have sent data before they receive our
+    /// close frame, so we should still pass those to client code, hence ClosedByUs is valid.
+    fn can_read(&self) -> bool {
+        match *self {
+            WebSocketState::Active     |
+            WebSocketState::ClosedByUs => true,
             _ => false,
         }
     }
