2ca6b992cf3df80e41ad7ad5b8a8d55e1a095c98
n4n5 <its.just.n4n5@gmail.com>
2024-04-23 18:06:32 +0200

    fix: clearer test

--- tests/url_feature.rs
+++ tests/url_feature.rs
@@ -2,7 +2,9 @@
 #![cfg(feature = "url")]
 
 use std::{
+    assert,
     net::TcpListener,
+    println,
     process::exit,
     thread::{sleep, spawn},
     time::Duration,
@@ -10,14 +12,15 @@ use std::{
 use tungstenite::{
     accept_hdr, connect,
     handshake::server::{Request, Response},
-    Error, Message,
 };
 
 /// Test for write buffering and flushing behaviour.
 #[test]
 fn test_with_url() {
     env_logger::init();
-    let url = url::Url::parse("ws://127.0.0.1:3013/socket").unwrap();
+    // notice the use of url::Url instead of a string
+    // notice the feature url is activated
+    let url = url::Url::parse("ws://127.0.0.1:3013").unwrap();
 
     spawn(|| {
         sleep(Duration::from_secs(5));
@@ -28,43 +31,16 @@ fn test_with_url() {
     let server = TcpListener::bind("127.0.0.1:3013").unwrap();
 
     let client_thread = spawn(move || {
-        let (mut client, _) = connect(url).unwrap();
-        client.send(Message::Text("Hello WebSocket".into())).unwrap();
-
-        let message = client.read().unwrap(); // receive close from server
-        assert!(message.is_close());
-
-        let err = client.read().unwrap_err(); // now we should get ConnectionClosed
-        match err {
-            Error::ConnectionClosed => {}
-            _ => panic!("unexpected error: {:?}", err),
-        }
+        let conn = connect(url);
+        assert!(conn.is_ok());
     });
 
-    let callback = |req: &Request, response: Response| {
-        println!("Received a new ws handshake");
-        println!("The request's path is: {}", req.uri().path());
-        Ok(response)
-    };
-
     let client_handler = server.incoming().next().unwrap();
-    let mut client_handler = accept_hdr(client_handler.unwrap(), callback).unwrap();
 
-    client_handler.close(None).unwrap(); // send close to client
+    let closing =
+        accept_hdr(client_handler.unwrap(), |_: &Request, r: Response| Ok(r)).unwrap().close(None);
+    assert!(closing.is_ok());
 
-    // This read should succeed even though we already initiated a close
-    let message = client_handler.read().unwrap();
-    assert_eq!(message.into_data(), b"Hello WebSocket");
-
-    assert!(client_handler.read().unwrap().is_close()); // receive acknowledgement
-
-    let err = client_handler.read().unwrap_err(); // now we should get ConnectionClosed
-    match err {
-        Error::ConnectionClosed => {}
-        _ => panic!("unexpected error: {:?}", err),
-    }
-
-    drop(client_handler);
-
-    client_thread.join().unwrap();
+    let result = client_thread.join();
+    assert!(result.is_ok());
 }
